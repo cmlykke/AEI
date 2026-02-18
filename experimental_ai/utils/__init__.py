8c7063eaa3ddeb8ff6472066d41b195acdb806a1
Weiqifan1 <cmlykke@hotmail.com>
2026-02-18 11:57:58 +0100

    removed dead code

--- experimental_ai/utils/__init__.py
+++ experimental_ai/utils/__init__.py
@@ -1,15 +1,1 @@
 
-"""
-experimental_ai.utils
-
-Internal utility package for the experimental bot.
-
-Public API policy:
-- External code should import ONLY from experimental_ai.utils.eval_glue
-  (or from this package, which re-exports the same glue functions).
-- Other modules in this folder are considered internal implementation details.
-"""
-
-from .eval_glue import GlueConfig, get_eval_step_move
-
-__all__ = ["GlueConfig", "get_eval_step_move"]
